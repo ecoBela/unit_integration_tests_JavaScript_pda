410fd09aade29986d0b7620c16d45e6b50485a2d
Bela <belahamid@gmail.com>
2021-03-16 11:17:49 +0000

    added additional operation to test concatenation: test passed

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -42,7 +42,10 @@ describe('App.vue', () => {
     wrapper.vm.operatorClick('+')
     wrapper.vm.numberClick('2')
     wrapper.vm.operatorClick('=')
-    expect(wrapper.vm.runningTotal).to.equal(7)
+    wrapper.vm.operatorClick('+')
+    wrapper.vm.numberClick('3')
+    wrapper.vm.operatorClick('=')
+    expect(wrapper.vm.runningTotal).to.equal(10)
   });
   xit('can chain multiple operations together', () =>{
     const wrapper = shallowMount(App)
