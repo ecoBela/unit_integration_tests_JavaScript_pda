0e12b5ab44d98570d05894bd62d2c9284b03c001
Bela <belahamid@gmail.com>
2021-03-16 15:11:04 +0000

    two more integration tests using Cypress have passed

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -12,15 +12,28 @@ describe('calculator', () => {
 
   it('should update display of running total', () =>{
     cy.get('#number3').click();
-    // cy.get('.display').should('contain', '3')
     cy.get('#operator_add').click();
     cy.get('#number2').click();
-    // cy.get('display').should('contain', '2')
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '5')
   });
-  xit('should update display with result of an operation');
-  xit('should chain multiple operations together');
+  it('should update display with result of an operation', () =>{
+    cy.get('#number6').click();
+    cy.get('#operator_add').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '10')
+  });
+  it('should chain multiple operations together', () =>{
+    cy.get('#number6').click();
+    cy.get('#operator_add').click();
+    cy.get('#number4').click()
+    cy.get('#operator_multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '20')
+  });
+
   xit('should give expected output for range of numbers e.g. negative, decimals');
   xit('should do XXXXXX in THIS exceptional circumstance');
 
