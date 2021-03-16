7ff084ccd00b737ab64e480db2c797adb5112de8
Bela <belahamid@gmail.com>
2021-03-16 10:47:20 +0000

    test passed: can multiply)

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -24,7 +24,12 @@ describe('App.vue', () => {
     expect(wrapper.vm.runningTotal).to.equal(3)
   });
 
-  xit('can multiply two numbers');
+  it('can multiply two numbers', () =>{
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  });
   xit('can divide a number into another');
   xit('can concatenate multiple number button clicks');
   xit('can chain multiple operations together');
