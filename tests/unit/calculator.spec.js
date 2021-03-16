9d2d6150ec705a992ac1866a965324e90d35320b
Bela <belahamid@gmail.com>
2021-03-16 10:41:46 +0000

    test passed: it can add two numbers

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
+import Calculator from '@/components/Calculator.vue'
 
 describe('App.vue', () => {
   it('enterNum changes running total', () => {
@@ -8,21 +9,24 @@ describe('App.vue', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
-  })
-})
-
-describe('Calculator.vue', () => {
-  it('can add two numbers', () =>{
-
   });
-  it('can subtract one number from another');
-  it('can multiply two numbers');
-  it('can divide a number into another');
-  it('can concatenate multiple number button clicks');
-  it('can chain multiple operations together');
-  it('can clear the running total without affecting the calculation');
+  it('can add two numbers', () =>{
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('1');
+    expect(wrapper.vm.runningTotal).to.equal(5)      
+  });
+
+  xit('can subtract one number from another');
+  
+  xit('can multiply two numbers');
+  xit('can divide a number into another');
+  xit('can concatenate multiple number button clicks');
+  xit('can chain multiple operations together');
+  xit('can clear the running total without affecting the calculation');
 
 })
+
 
 
 
